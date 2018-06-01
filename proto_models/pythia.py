ab16b48cde7a919d2d4f71ed5eaf9422a211f9ed
josemodena <jose.modena@gmail.com>
2018-06-01 18:49:23 +0100

    First working version. Lots of bugs fixed. Forecasts based on actual_sales only.

--- proto_models/pythia.py
+++ proto_models/pythia.py
@@ -16,14 +16,15 @@ class Data_Transformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
         X['target_year'] = X['target_month'].dt.year
         X['target_qtr'] = X['target_month'].apply(lambda x: (x.month-1)//3 + 1)
-        X['target_month'] = X['target_month'].dt.month - ((df['target_month'].dt.month-1)//3)*3
+        X['target_mth'] = X['target_month'].dt.month - ((X['target_month'].dt.month-1)//3)*3
 
-        actual_sales_qtr = pd.DataFrame(X.groupby(['market', 'vehicle_line', 'target_year', 'target_qtr'])['actual_sales'].apply(array).apply(sum))
+        X['actual_sales'] = y
+        actual_sales_qtr = pd.DataFrame(X.groupby(['market', 'vehicle_line', 'target_year', 'target_qtr'])['actual_sales'].apply(sum))
         actual_sales_qtr.rename(columns={'actual_sales': 'actual_sales_qtr'}, inplace=True)
         X = X.join(actual_sales_qtr, on=['market', 'vehicle_line', 'target_year', 'target_qtr'])
 
         X['theta'] = X['actual_sales'] / X['actual_sales_qtr']
-        self.thetas = dict(tuple(X[['market', 'vehicle_line', 'target_qtr', 'target_month', 'theta']].groupby(['market', 'vehicle_line', 'target_qtr', 'target_month'])['theta']))
+        self.thetas = dict(tuple(X[['market', 'vehicle_line', 'target_qtr', 'target_mth', 'theta']].groupby(['market', 'vehicle_line', 'target_qtr', 'target_mth'])['theta']))
 
         X['IHS_t_vl_mth'] = X['IHS_t_vl'] * X['theta']
         X['IHS_t_mth'] = X['IHS_t'] * X['theta']
@@ -32,14 +33,14 @@ class Data_Transformer(BaseEstimator, TransformerMixin):
     def transform(self, X):
         X['target_year'] = X['target_month'].dt.year
         X['target_qtr'] = X['target_month'].apply(lambda x: (x.month-1)//3 + 1)
-        X['target_month'] = X['target_month'].dt.month - ((df['target_month'].dt.month-1)//3)*3
+        X['target_mth'] = X['target_month'].dt.month - ((X['target_month'].dt.month-1)//3)*3
 
         X['theta'] = np.nan
         for i in range(0, len(X)):
             market = X.iloc[i]['market']
             vehicle_line = X.iloc[i]['vehicle_line']
             qtr = X.iloc[i]['target_qtr']
-            month = X.iloc[i]['target_month']
+            month = X.iloc[i]['target_mth']
             theta = np.mean(self.thetas[market, vehicle_line, qtr, month])
             X.iloc[i, -1] = theta
             self.thetas[market, vehicle_line, qtr, month].append(pd.Series(theta))
@@ -53,18 +54,15 @@ class Pythia(BaseEstimator, RegressorMixin):
         self.params = params
         self.granularity = granularity
 
-def create_ihs_month_forecast(df):
-    df['IHS_t_vl_mth'] = df['IHS_t_vl'] * df['theta']
-    df['IHS_t_mth'] = df['IHS_t'] * df['theta']
-    return df
-
     def fit(self, X, y):
+        self.predictors_num = 0
+        X['y'] = y
         X_dict = dict(tuple(X.groupby(self.granularity))) #create a dictionary of dataframes according to the granularity of model. Here, for each market and vehicle line we have a df
-        self.model = []
+        self.model = {}
         for k,v in X_dict.items():
             self.model[k] = Prophet()
             ds = v['target_month']
-            target = np.log(y)
+            target = np.log(v['y'])
             prophet_df = pd.DataFrame({'ds': ds, 'y': target})
             self.model[k].fit(prophet_df) #create a model for each k (market and vehicle line)
         return self
@@ -77,12 +75,9 @@ def create_ihs_month_forecast(df):
             prophet_df = pd.DataFrame({'ds': ds})
             pred = self.model[k].predict(prophet_df)['yhat'] #predict for each market vehicle line using the appropriate model trained in 'fitz
             pred.index = v.index
-        predictions = pd.concat(pred)
-        return predictions
+            predictions.append(pred)
+        return pd.concat(predictions)
 
 def construct_Pipline():
-    data_transformer = Data_Transformer()
-    model = Pythia(granularity=['market', 'vehicle_line'])
-    pline = Pipeline([('data_transformer', data_transformer),
-                    ('model', model)])
-    return pline
+    return Pipeline([('pythia_transformer', Data_Transformer()),
+                    ('pythia_model', Pythia(granularity=['market', 'vehicle_line']))])
